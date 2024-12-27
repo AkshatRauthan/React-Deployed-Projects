3d9a74810e7dacd1adb4013ed7bf3f4794a5b79f
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-12-27 19:50:23 +0530

    Added Mobile Responsiveness.

--- 01. Tic Tac Toe/src/components/Result.jsx
+++ 01. Tic Tac Toe/src/components/Result.jsx
@@ -1,21 +1,25 @@
-import "../styles/Result.css"
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Winner from './Winner.jsx';
+
 import Draw from './Draw.jsx';
+import  "../styles/Result.css";
+import Winner from './Winner.jsx';
+import Button from '@mui/material/Button';
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 let Result = (props) => {
     let {winner, restartGame, clearScore} = props;
-    
+
     let isDraw = false;
     if (winner == "Draw") isDraw = true;
 
     return (
         <div className="showResult">
+            <Fireworks autorun={{ speed: 1}} />
+            <Fireworks autorun={{ speed: 1}} />
+            <Fireworks autorun={{ speed: 1}} /> 
             <div className="resultContainer">
                 {isDraw && <Draw />}
-                {!isDraw && <Winner winner={winner}/>}
-                <div>
+                {!isDraw && <Winner winner={winner} />}
+                <div className="resultButtonsContainer">
                     <Button variant="outlined" size="large" 
                     onClick={() => restartGame()}>Restart Game</Button><br /><br />
                     

3316f61454ab7c9aed722cdeeaa84c864950e6a7
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-12-27 22:13:05 +0530

    Final Project Configuration.

--- 01. Tic Tac Toe/src/components/Result.jsx
+++ 01. Tic Tac Toe/src/components/Result.jsx
@@ -2,20 +2,35 @@
 import Draw from './Draw.jsx';
 import  "../styles/Result.css";
 import Winner from './Winner.jsx';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 let Result = (props) => {
     let {winner, restartGame, clearScore} = props;
 
+    let [flag, setFlag] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFlag(true);
+            setTimeout(() => {
+                setFlag(false);
+            }, 1000);
+        }, 90);
+    }, []);
+
     let isDraw = false;
     if (winner == "Draw") isDraw = true;
 
     return (
         <div className="showResult">
-            <Fireworks autorun={{ speed: 1}} />
-            <Fireworks autorun={{ speed: 1}} />
-            <Fireworks autorun={{ speed: 1}} /> 
+            {flag && <>
+                <Fireworks autorun={{ speed: 1}} />
+                <Fireworks autorun={{ speed: 1}} />
+                <Fireworks autorun={{ speed: 1}} />
+                <Fireworks autorun={{ speed: 1}} />
+            </>}
             <div className="resultContainer">
                 {isDraw && <Draw />}
                 {!isDraw && <Winner winner={winner} />}
